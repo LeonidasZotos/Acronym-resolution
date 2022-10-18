82edf42e91fda5fa2764740931ff936761e4a216
manonheinhuis <m.heinhuis@student.rug.nl>
2022-10-18 16:12:46 +0200

    "Added semantic expansion start, currently broken"

--- src/semanticExpansion.py
+++ src/semanticExpansion.py
@@ -1,6 +1,52 @@
+import re
+import sys
+import requests
+import urllib3
 
+## This function sets up the queries, using the input data.
+def create_query(X):
+    query = '''
+        ?''' + X + ''' schema:description ?itemdesc.
+        FILTER(LANG(?itemdesc) = "en")
+    '''
+    # '''SELECT ?answerLabel WHERE {
+    #     wd:''' + X + ''' wdt:''' + Y + ''' ?answer.
+    #     SERVICE wikibase:label {
+    #         bd:serviceParam wikibase:language "en" .
+    #     }
+    # }'''
+    return query
 
-def expandSemantically(text, acronymsAndTheirSentences):
+## This function will execute the query and print the 'answer' the data of the query links to.
+def run_query(query):
+	url = 'https://query.wikidata.org/sparql'
+     # TODO: it breaks here because of the query not being JSON
+	data = requests.get(url, params={'query': query, 'format': 'json'}).json()
+	final_results = []
+	for item in data['results']['bindings']:
+		final_results.append(item['answerLabel']['value'])
+	return final_results
+
+## This function finds the wd SPARQL code for a natural word. 
+def find_entity(line):
+    url = 'https://www.wikidata.org/w/api.php'
+    params = {'action':'wbsearchentities',
+          'limit':10,
+          'language':'en',
+          'format':'json'}
+    params['search'] = line.rstrip()
+    json = requests.get(url,params).json() 
+    answerList = []
+    for result in json['search']:
+        answerList.append(result['id'])
+    return answerList                           
+
+def expandSemantically(text, acronym):
     #I guess it needs acronymsAndTheirSentences to see which acronyms are to be expanded.
+    entity =  find_entity(acronym)
+    query = create_query(entity[0])
+    result = run_query(query)
     
-    return text
+    # TODO: Add result to text function
+    print(result)
+    # return text
